8f1e24fd6a3cbe141b786c0e420a9c561c939c96
Campos <100108519+camposeduardo@users.noreply.github.com>
2024-04-30 15:14:33 -0300

    Initial commit

---
+++ src/main/java/com/camposeduardo/cinesearch/CinesearchApplication.java
@@ -0,0 +1,13 @@
+package com.camposeduardo.cinesearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinesearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinesearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/camposeduardo/cinesearch/CinesearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camposeduardo.cinesearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinesearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
